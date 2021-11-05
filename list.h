dd47175e96fac037e59af980aaae5951c72bfda1
nerett <pt-mvb@inbox.lv>
2021-11-05 17:10:31 +0300

    initial

---
+++ list.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+
+#include <assert.h>
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+
+	return 0;
+}
