92e8278b69b25c775deb0efcfe0f99f46828a2d9
nerett <pt-mvb@inbox.lv>
2021-11-05 18:01:42 +0300

    added insertion func

--- list.cpp
+++ list.cpp
@@ -1,1 +1,101 @@
 #include "list.h"
+
+
+void ListCtor( List* some_list )
+{
+	if( some_list->is_initialized == true )
+	{
+		return;
+	}
+
+	some_list->N_elements = 0;
+	some_list->capacity = START_LIST_CAPACITY;
+
+	some_list->head = 0;
+	some_list->tail = 0;
+
+	some_list->next = ( int* )calloc( some_list->N_elements, sizeof( int ) );
+	some_list->data = ( list_element_t* )calloc( some_list->N_elements, sizeof( list_element_t ) );
+
+	for( int i = 1; i < some_list->capacity; i++ )
+	{
+		some_list->next[i] = -1;
+	}
+
+	some_list->is_initialized = true;
+}
+
+
+
+void ListDtor( List* some_list )
+{
+	if( some_list->is_initialized == false )
+	{
+		return;
+	}
+
+	some_list->is_initialized = false;
+
+	free( some_list->data );
+	free( some_list->next );
+
+	some_list->N_elements = NO_ELEMENTS;
+	some_list->capacity = NO_ELEMENTS;
+	some_list->head = 0;
+	some_list->tail = 0;
+
+	some_list->data = NULL;
+	some_list->next = NULL;
+}
+
+
+
+int lst_insert( List* some_list, list_element_t value, int place )
+{
+	if( place == LST_BACK )
+	{
+		printf( "fuck you, leatherman!\n" );
+		abort();
+		//some calculations
+	}
+
+	int free_place = find_first_free_place( some_list );
+	if( free_place == 0 )
+	{
+		return {};
+	}
+
+	some_list->data[free_place] = value; //записываем значение
+
+	some_list->next[free_place] = some_list->next[place]; //новый элемент ссылается на следующий
+	some_list->next[place] = free_place; //предшествующий элемент ссылается на новый элемент
+
+	some_list->N_elements++;
+
+	if( some_list->next[free_place] == 0 )
+	{
+		some_list->head == 1;
+	}
+	if( some_list->tail < free_place )
+	{
+		some_list->tail = free_place;
+	}
+
+	return free_place;
+}
+
+
+
+int find_first_free_place( List* some_list )
+{
+	int counter = 0;
+	for( counter; counter < some_list->capacity - 1; counter++ )
+	{
+		if( some_list->next[counter] == -1 )
+		{
+			break;
+		}
+	}
+
+	return counter;
+}
--- list.h
+++ list.h
@@ -3,8 +3,37 @@
 
 
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
+typedef int list_element_t;
+
+const int START_LIST_CAPACITY = 256; //0 is unused
+const int INT_POISON = 0xAB0BA;
+const int NO_ELEMENTS = 0;
+const int LST_BACK = 0;
+
+struct List
+{
+	list_element_t* data = NULL;
+	int* next = NULL;
+
+	int N_elements = 0;
+	int capacity = 0;
+
+	int head = 0;
+	int tail = 0;
+
+	bool is_initialized = false;
+};
+
+void ListCtor( List* some_list );
+void ListDtor( List* some_list );
+
+int lst_insert( List* some_list, list_element_t value, int place = LST_BACK );
+
+int find_first_free_place( List* some_list );
 
 
 #endif
